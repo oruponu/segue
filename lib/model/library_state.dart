cc29edfa05672478bdbb129e3e0854dcc5af632c
oruponu <oruponu@outlook.com>
2026-02-01 00:19:26 +0900

    再生中の楽曲情報のフィールドを追加

--- lib/model/library_state.dart
+++ lib/model/library_state.dart
@@ -4,22 +4,26 @@ class LibraryState {
   final List<AudioMetadata> playlist;
   final bool isLoading;
   final String? selectedDirectory;
+  final AudioMetadata? playingMetadata;
 
   LibraryState({
     this.playlist = const [],
     this.isLoading = false,
     this.selectedDirectory,
+    this.playingMetadata,
   });
 
   LibraryState copyWith({
     List<AudioMetadata>? playlist,
     bool? isLoading,
     String? selectedDirectory,
+    AudioMetadata? playingMetadata,
   }) {
     return LibraryState(
       playlist: playlist ?? this.playlist,
       isLoading: isLoading ?? this.isLoading,
       selectedDirectory: selectedDirectory ?? this.selectedDirectory,
+      playingMetadata: playingMetadata ?? this.playingMetadata,
     );
   }
 }
