a72ecf79a4308dbbaad3284aacba522dbf1752a1
oruponu <oruponu@outlook.com>
2026-01-31 17:55:28 +0900

    フォルダ選択機能を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
@@ -29,11 +30,23 @@ class PlayerScreen extends StatefulWidget {
 
 class _PlayerScreenState extends State<PlayerScreen> {
   late AudioPlayer _player;
+  String? _selectedDirectory;
 
   @override
   void initState() {
     super.initState();
     _player = AudioPlayer();
+  }
+
+  Future<void> _selectDirectory() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    if (selectedDirectory == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedDirectory = selectedDirectory;
+    });
   }
 
   @override
@@ -46,53 +59,72 @@ class _PlayerScreenState extends State<PlayerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            StreamBuilder<PlayerState>(
-              stream: _player.playerStateStream,
-              builder: (context, snapshot) {
-                final playerState = snapshot.data;
-                final processingState = playerState?.processingState;
-                final playing = playerState?.playing;
+      floatingActionButton: FloatingActionButton(
+        onPressed: _selectDirectory,
+        child: const Icon(Icons.folder_open),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_selectedDirectory ?? "フォルダを選択してください"),
+              ),
 
-                if (processingState == ProcessingState.loading ||
-                    processingState == ProcessingState.buffering) {
-                  return const CircularProgressIndicator();
-                } else if (playing != true) {
-                  return IconButton(
-                    icon: const Icon(Icons.play_arrow),
-                    iconSize: 64.0,
-                    onPressed: _player.play,
-                  );
-                } else if (processingState != ProcessingState.completed) {
-                  return IconButton(
-                    icon: const Icon(Icons.pause),
-                    iconSize: 64.0,
-                    onPressed: _player.pause,
-                  );
-                } else {
-                  return IconButton(
-                    icon: const Icon(Icons.replay),
-                    iconSize: 64.0,
-                    onPressed: () => _player.seek(Duration.zero),
-                  );
-                }
-              },
-            ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    StreamBuilder<PlayerState>(
+                      stream: _player.playerStateStream,
+                      builder: (context, snapshot) {
+                        final playerState = snapshot.data;
+                        final processingState = playerState?.processingState;
+                        final playing = playerState?.playing;
 
-            const SizedBox(height: 20),
-            IconButton(
-              icon: const Icon(Icons.skip_next),
-              iconSize: 48,
-              onPressed: () {
-                if (_player.hasNext) {
-                  _player.seekToNext();
-                }
-              },
-            ),
-          ],
+                        if (processingState == ProcessingState.loading ||
+                            processingState == ProcessingState.buffering) {
+                          return const CircularProgressIndicator();
+                        } else if (playing != true) {
+                          return IconButton(
+                            icon: const Icon(Icons.play_arrow),
+                            iconSize: 64.0,
+                            onPressed: _player.play,
+                          );
+                        } else if (processingState !=
+                            ProcessingState.completed) {
+                          return IconButton(
+                            icon: const Icon(Icons.pause),
+                            iconSize: 64.0,
+                            onPressed: _player.pause,
+                          );
+                        } else {
+                          return IconButton(
+                            icon: const Icon(Icons.replay),
+                            iconSize: 64.0,
+                            onPressed: () => _player.seek(Duration.zero),
+                          );
+                        }
+                      },
+                    ),
+
+                    const SizedBox(height: 20),
+                    IconButton(
+                      icon: const Icon(Icons.skip_next),
+                      iconSize: 48,
+                      onPressed: () {
+                        if (_player.hasNext) {
+                          _player.seekToNext();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
