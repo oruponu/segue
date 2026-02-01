b6d418d7da011d945cafaef21a2f933ad34fb1af
oruponu <oruponu@outlook.com>
2026-02-01 22:12:48 +0900

    `AudioPlayer` をラップしたハンドラを実装

---
+++ lib/providers/audio_handler_provider.dart
@@ -0,0 +1,69 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio/just_audio.dart';
+import 'audio_player_provider.dart';
+
+final audioHandlerProvider = Provider<AudioHandler>((ref) {
+  final player = ref.watch(audioPlayerProvider);
+  return AudioHandler(player);
+});
+
+final mediaItemProvider = StreamProvider<MediaItem?>((ref) {
+  final handler = ref.watch(audioHandlerProvider);
+  return handler.mediaItem;
+});
+
+class AudioHandler extends BaseAudioHandler {
+  final AudioPlayer _player;
+
+  AudioHandler(this._player) {
+    _player.playerStateStream.listen((state) {
+      playbackState.add(
+        playbackState.value.copyWith(
+          playing: state.playing,
+          controls: [
+            MediaControl.skipToPrevious,
+            state.playing ? MediaControl.pause : MediaControl.play,
+            MediaControl.stop,
+            MediaControl.skipToNext,
+          ],
+          androidCompactActionIndices: const [1, 3],
+          systemActions: {MediaAction.seek},
+          updatePosition: _player.position,
+          bufferedPosition: _player.bufferedPosition,
+          speed: _player.speed,
+        ),
+      );
+    });
+
+    _player.sequenceStateStream.listen((sequenceState) {
+      final currentItem = sequenceState.currentSource?.tag as MediaItem?;
+      if (currentItem != null) {
+        mediaItem.add(currentItem);
+      }
+    });
+  }
+
+  @override
+  Future<void> play() => _player.play();
+
+  @override
+  Future<void> pause() => _player.pause();
+
+  @override
+  Future<void> updateQueue(List<MediaItem> queue) async {
+    this.queue.add(queue);
+    final audioSource = queue
+        .map((item) => AudioSource.uri(Uri.parse(item.id), tag: item))
+        .toList();
+    await _player.setAudioSources(audioSource);
+  }
+
+  @override
+  Future<void> skipToQueueItem(int index) async {
+    await _player.seek(Duration.zero, index: index);
+  }
+
+  @override
+  Future<void> seek(Duration position) => _player.seek(position);
+}
