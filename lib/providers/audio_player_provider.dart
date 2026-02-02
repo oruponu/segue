676a5766acbd87fe681c1d666722b807a3f3f069
oruponu <oruponu@outlook.com>
2026-02-02 21:21:51 +0900

    不要になったProviderを削除

--- lib/providers/audio_player_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:just_audio/just_audio.dart';
-
-final audioPlayerProvider = Provider<AudioPlayer>((ref) {
-  final player = AudioPlayer();
-  ref.onDispose(() => player.dispose());
-  return player;
-});
