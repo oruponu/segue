784eaa59a161b7b28abbac751a576cb3c5c2d443
oruponu <oruponu@outlook.com>
2026-02-03 21:05:58 +0900

    `dispose` メソッドを実装

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -20,6 +20,12 @@ class _MainScreenState extends ConsumerState<MainScreen> {
       DraggableScrollableController();
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     const double miniPlayerHeight = 72.0;
     final double screenHeight = MediaQuery.of(context).size.height;
