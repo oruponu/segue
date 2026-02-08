c4d409a50323a06f3551be823cf940aa9d9a835c
oruponu <oruponu@outlook.com>
2026-02-08 16:48:00 +0900

    プレーヤー画面のStateにテンポとキーのフィールドを追加

--- lib/model/player_state.dart
+++ lib/model/player_state.dart
@@ -4,22 +4,34 @@ class PlayerState {
   final String? selectedDirectory;
   final bool isLoading;
   final MediaItem? playingMediaItem;
+  final bool isAnalyzing;
+  final double? bpm;
+  final String? key;
 
   PlayerState({
     this.selectedDirectory,
     this.isLoading = false,
     this.playingMediaItem,
+    this.isAnalyzing = false,
+    this.bpm,
+    this.key,
   });
 
   PlayerState copyWith({
     String? selectedDirectory,
     bool? isLoading,
     MediaItem? playingMediaItem,
+    bool? isAnalyzing,
+    double? bpm,
+    String? key,
   }) {
     return PlayerState(
       selectedDirectory: selectedDirectory ?? this.selectedDirectory,
       isLoading: isLoading ?? this.isLoading,
       playingMediaItem: playingMediaItem ?? this.playingMediaItem,
+      isAnalyzing: isAnalyzing ?? this.isAnalyzing,
+      bpm: bpm ?? this.bpm,
+      key: key ?? this.key,
     );
   }
 }
