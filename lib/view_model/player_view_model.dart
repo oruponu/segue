ecfc25674bd3a691d3e5f08744f266654a5c0f6b
oruponu <oruponu@outlook.com>
2026-02-08 17:04:00 +0900

    曲が切り替わったときにテンポ解析とキー解析を実行

--- lib/view_model/player_view_model.dart
+++ lib/view_model/player_view_model.dart
@@ -1,6 +1,9 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:segue/model/player_state.dart';
 import 'package:segue/providers/audio_handler_provider.dart';
+import 'package:segue/src/rust/api/audio_analysis.dart';
 
 final playerViewModelProvider = NotifierProvider<PlayerViewModel, PlayerState>(
   () {
@@ -12,9 +15,22 @@ class PlayerViewModel extends Notifier<PlayerState> {
   @override
   PlayerState build() {
     final handler = ref.watch(audioHandlerProvider);
-    handler.mediaItem.listen((item) {
-      state = state.copyWith(playingMediaItem: item);
-    });
+    handler.mediaItem
+        .distinct((previous, next) => previous?.id == next?.id)
+        .whereType<MediaItem>()
+        .listen((item) async {
+          state = PlayerState(playingMediaItem: item, isAnalyzing: true);
+          final result = await analyze(pathStr: item.id);
+          if (result == null) {
+            return;
+          }
+
+          state = state.copyWith(
+            bpm: result.bpm,
+            key: result.key,
+            isAnalyzing: false,
+          );
+        });
 
     return PlayerState(playingMediaItem: null);
   }
