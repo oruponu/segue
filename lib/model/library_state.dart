18b29d99eb81bfe3cea841169932d2f915721e4e
oruponu <oruponu@outlook.com>
2026-02-11 19:38:22 +0900

    ライブラリ画面のStateにスキャン進捗のフィールドを追加

--- lib/model/library_state.dart
+++ lib/model/library_state.dart
@@ -4,6 +4,8 @@ import 'package:segue/model/album.dart';
 class LibraryState {
   final List<MediaItem> playlist;
   final bool isLoading;
+  final int scanTotal;
+  final int scanProcessed;
   final List<Album> albums;
   final Album? selectedAlbum;
   final String? selectedDirectory;
@@ -13,6 +15,8 @@ class LibraryState {
   LibraryState({
     this.playlist = const [],
     this.isLoading = false,
+    this.scanTotal = 0,
+    this.scanProcessed = 0,
     this.albums = const [],
     this.selectedAlbum,
     this.selectedDirectory,
@@ -23,6 +27,8 @@ class LibraryState {
   LibraryState copyWith({
     List<MediaItem>? playlist,
     bool? isLoading,
+    int? scanTotal,
+    int? scanProcessed,
     List<Album>? albums,
     Album? selectedAlbum,
     bool clearSelectedAlbum = false,
@@ -33,6 +39,8 @@ class LibraryState {
     return LibraryState(
       playlist: playlist ?? this.playlist,
       isLoading: isLoading ?? this.isLoading,
+      scanTotal: scanTotal ?? this.scanTotal,
+      scanProcessed: scanProcessed ?? this.scanProcessed,
       albums: albums ?? this.albums,
       selectedAlbum: clearSelectedAlbum
           ? null
