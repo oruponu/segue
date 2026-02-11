347438e7cdc5392c6a48f7023119c7b4ab030a7c
oruponu <oruponu@outlook.com>
2026-02-11 21:06:47 +0900

    日本語ロケールの設定を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:segue/providers/audio_handler_provider.dart';
@@ -25,6 +26,12 @@ class SegueApp extends StatelessWidget {
       title: 'Segue',
       theme: ThemeData.dark(),
       home: const MainScreen(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('ja', 'JP')],
     );
   }
 }
