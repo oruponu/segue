157438e13754d49f7f3f5acf22bfa728646159ed
oruponu <oruponu@outlook.com>
2026-02-18 01:28:18 +0900

    `Essentia` のC++ブリッジを追加

---
+++ native/src/essentia_bridge.h
@@ -0,0 +1,34 @@
+#ifndef ESSENTIA_BRIDGE_H
+#define ESSENTIA_BRIDGE_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  float bpm;
+  float bpm_confidence;
+  int8_t key_note;   // 0-11 (C=0...B=11), -1 = unknown
+  int8_t key_scale;  // 0=major, 1=minor
+  float key_confidence;
+  int32_t error_code;  // 0=success, 1=cancelled, 2=decode error, 3=analysis error
+} EssentiaResult;
+
+typedef struct EssentiaCancelFlag EssentiaCancelFlag;
+
+EssentiaCancelFlag* essentia_cancel_flag_create(void);
+void essentia_cancel_flag_set(EssentiaCancelFlag* flag);
+void essentia_cancel_flag_destroy(EssentiaCancelFlag* flag);
+
+void essentia_init(void);
+void essentia_shutdown(void);
+
+EssentiaResult essentia_analyze(const char* path, EssentiaCancelFlag* cancel_flag);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ESSENTIA_BRIDGE_H
