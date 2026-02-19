59bff24b72cdf62cf1890c569fa920d8cfec00ee
oruponu <oruponu@outlook.com>
2026-02-20 02:45:27 +0900

    波形データを提供するProviderをFutureProviderに変更

--- lib/providers/waveform_provider.dart
+++ lib/providers/waveform_provider.dart
@@ -1,37 +1,32 @@
 import 'dart:io';
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_waveform/just_waveform.dart';
 import 'package:segue/providers/audio_handler_provider.dart';
 
-final waveformProvider = StreamProvider.autoDispose<Waveform?>((ref) async* {
-  final handler = ref.watch(audioHandlerProvider);
+final waveformProvider = FutureProvider.autoDispose<Waveform?>((ref) async {
+  final item = ref.watch(_mediaItemProvider).value;
+  if (item == null) return null;
 
-  await for (final item in handler.mediaItem) {
-    if (item == null) {
-      yield null;
-      continue;
+  final wavePath = item.extras?['wavePath'] as String?;
+  if (wavePath == null || wavePath.isEmpty) return null;
+
+  final file = File(wavePath);
+  if (!await file.exists()) {
+    try {
+      await JustWaveform.extract(
+        audioInFile: File(item.id),
+        waveOutFile: file,
+      ).drain();
+    } catch (_) {
+      return null;
     }
-
-    final wavePath = item.extras?['wavePath'] as String?;
-    if (wavePath == null || wavePath.isEmpty) {
-      yield null;
-      continue;
-    }
-
-    final file = File(wavePath);
-    if (!await file.exists()) {
-      yield null;
-      try {
-        await JustWaveform.extract(
-          audioInFile: File(item.id),
-          waveOutFile: file,
-        ).drain();
-      } catch (_) {
-        yield null;
-        continue;
-      }
-    }
-
-    yield await JustWaveform.parse(file);
   }
+
+  return await JustWaveform.parse(file);
+});
+
+final _mediaItemProvider = StreamProvider<MediaItem?>((ref) {
+  final handler = ref.watch(audioHandlerProvider);
+  return handler.mediaItem;
 });
